6cd1a18d524a2abe4b4ea076650f9da515d6717b
STBenji <stevenbenjumea9@gmail.com>
2024-02-06 23:25:45 -0500

    tecnologies added and working

--- src/components/sectionTwo.jsx
+++ src/components/sectionTwo.jsx
@@ -1,7 +1,51 @@
+import React from 'react'
+import { FaReact, FaGithub, FaGitSquare, FaNodeJs } from 'react-icons/fa'
+import { SiTailwindcss, SiTypescript, SiMysql } from 'react-icons/si'
+import { IoLogoJavascript } from 'react-icons/io'
+import { TbBrandReactNative } from 'react-icons/tb'
+
 export const SectionTwo = () => {
   return (
-    <main className="h-screen p-5">
-      <h1>Sobre mí</h1>
-    </main>
+    <section className="flex flex-col items-center p-14 h-fulls md:justify-center ">
+      <h2 className="text-3xl font-bold md:text-4xl text-lighter">Tecnologías</h2>
+      <article className="flex flex-wrap justify-center gap-8 mt-20">
+        <div className="flex flex-col items-center space-y-4 hover:text-[#61DAFB]">
+          <FaReact className="text-6xl" />
+          <p className="text-xl">ReactJs</p>
+        </div>
+        <div className="flex flex-col items-center space-y-4 hover:text-[#333333]">
+          <FaGithub className="text-6xl" />
+          <p className="text-xl">GitHub</p>
+        </div>
+        <div className="flex flex-col items-center space-y-4 hover:text-[#38B2AC]">
+          <SiTailwindcss className="text-6xl" />
+          <p className="text-xl">Tailwind</p>
+        </div>
+        <div className="flex flex-col items-center space-y-4 hover:text-[#F7DF1E]">
+          <IoLogoJavascript className="text-6xl" />
+          <p className="text-xl">JavaScript</p>
+        </div>
+        <div className="flex flex-col items-center space-y-4 hover:text-[#68A063]">
+          <FaNodeJs className="text-6xl" />
+          <p className="text-xl">NodeJs</p>
+        </div>
+        <div className="flex flex-col items-center space-y-4 hover:text-[#007ACC]">
+          <SiTypescript className="text-6xl" />
+          <p className="text-xl">TypeScript</p>
+        </div>
+        <div className="flex flex-col items-center space-y-4 hover:text-[#F05032]">
+          <FaGitSquare className="text-6xl" />
+          <p className="text-xl">Git</p>
+        </div>
+        <div className="flex flex-col items-center space-y-4 hover:text-[#00758F]">
+          <SiMysql className="text-6xl" />
+          <p className="text-xl">MySQL</p>
+        </div>
+        <div className="flex flex-col items-center space-y-4 hover:text-[#61DAFB]">
+          <TbBrandReactNative className="text-6xl" />
+          <p className="text-xl">React Native</p>
+        </div>
+      </article>
+    </section>
   )
 }
