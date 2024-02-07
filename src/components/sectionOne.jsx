1b7da82d0ea8facc2a18d0df7c16ca79e33b3f75
STBenji <stevenbenjumea9@gmail.com>
2024-02-07 16:06:17 -0500

    first section finished, animations need to be added

--- src/components/sectionOne.jsx
+++ src/components/sectionOne.jsx
@@ -1,15 +1,16 @@
 import { Button } from '@nextui-org/react'
 import { FaGithub } from 'react-icons/fa6'
 import { CiLinkedin } from 'react-icons/ci'
+import { IoIosArrowDown } from 'react-icons/io'
 
 export const SectionOne = () => {
   return (
     <section className="flex justify-center h-screen px-4">
-      <header className="mt-[7rem] flex flex-col items-center gap-[16px] text-center">
+      <header className="mt-[7rem] flex flex-col items-center gap-[16px] text-center relative">
         <img src="imagePersonal.jpg" alt="" className="object-cover w-32 h-32 rounded-full" />
-        <h1 className="text-2xl font-bold md:text-5xl">¡¡Hola👋!! mi nombre es Stiven.</h1>
+        <h1 className="text-2xl font-bold md:text-5xl">¡¡Hola👋!! Mi nombre es Stiven.</h1>
         <h2 className="text-xl font-semibold tracking-wide md:text-3xl">
-          Desarrollador web <strong className="text-default"> Full Stack </strong>
+          Desarrollador web <strong className="text-lighter"> Full Stack </strong>
         </h2>
         <p className="md:w-[75ch] text-[#999]">Cuento con las habilidades para liderar equipos de desarrollo. Me distingo por mi actitud positiva, trabajo excelente en equipo y pasión por el aprendizaje. Poseo una capacidad sobresaliente para crear interfaces amigables para el usuario y me adapto fácilmente a diversos entornos de trabajo.</p>
         <div className="flex gap-3">
@@ -29,6 +30,9 @@ export const SectionOne = () => {
           </Button>
         </div>
       </header>
+      <div className="absolute text-3xl bottom-6 animate-bounce">
+        <IoIosArrowDown />
+      </div>
     </section>
   )
 }
